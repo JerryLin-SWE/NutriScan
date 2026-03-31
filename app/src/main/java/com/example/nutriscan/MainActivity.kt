6d5e8e91e57e3b1017c6ec36870f5aa0ac025196
BryanR227 <tuq78130@temple.edu>
2026-03-31 18:10:39 -0400

    database testing

--- app/src/main/java/com/example/nutriscan/MainActivity.kt
+++ app/src/main/java/com/example/nutriscan/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.nutriscan
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val db = Firebase.firestore
+
+
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "age" to 20
+        )
+
+// Add a new document with a generated ID
+        db.collection("usersTest")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
     }
 }

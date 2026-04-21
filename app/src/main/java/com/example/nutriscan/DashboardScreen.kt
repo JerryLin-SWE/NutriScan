567cae173640483284c984352a4779b70b0340bc
BryanR227 <tuq78130@temple.edu>
2026-04-21 14:29:44 -0400

    Cleaned up unsued imports, scopes, and variables.

--- app/src/main/java/com/example/nutriscan/DashboardScreen.kt
+++ app/src/main/java/com/example/nutriscan/DashboardScreen.kt
@@ -11,10 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
--- app/src/main/java/com/example/nutriscan/LoginScreen.kt
+++ app/src/main/java/com/example/nutriscan/LoginScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
-import kotlinx.coroutines.launch
-
 
 @Composable
 fun LoginScreen(
